ce7666ac38512a06f23b57d93053da2f47eace1d
nikita <nikita.lavrentyev@mysky.com>
2023-11-25 22:54:14 +0300

    feat:add sections offer and favorite

--- coffee-house/main.js
+++ coffee-house/main.js
@@ -1,19 +1,35 @@
 import './styles/index.scss'
 import Button from "./components/Button/Button.js";
-import Header from "./components/Header/Header.js";
 import MenuItem from "./components/MenuItem/MenuItem.js";
+
+const addElement = (parentSelector, element) => {
+    const parent = document.querySelector(parentSelector);
+    parent.append(element);
+}
+
+const createImgElement = (src, className, alt) => {
+    const img = document.createElement('img');
+    img.src = src;
+    img.className = className;
+    img.alt = alt;
+
+    return img;
+}
 
 
 window.addEventListener('DOMContentLoaded', function () {
-
-    new Header("#app").render();
     new MenuItem(".menu", "Menu", "./public/coffee-cup.svg", "coffee-cup").render();
 
 
-    // new Button("primary", "#app", "menu").render();
+    new Button("primary", ".wrapper", "Menu").render();
+
+    new Button("icon_dark", ".slider", "", "./public/arrow-left.svg").render();
+    const imgGlass = createImgElement("./public/coffee-slider-1.svg", "glass", "glass");
+    addElement(".slider", imgGlass);
+    new Button("icon_dark", ".slider", "", "./public/arrow-right.svg").render();
+
     // new Button("secondary", "#app", "menu").render();
-    // new Button("icon_dark", "#app", "", "./public/arrow-right.svg").render();
-    // new Button("icon_light", "#app", "", "./public/arrow-right.svg").render();
+    //new Button("icon_dark", ".slider", "", "./public/arrow-right.svg").render();
 
 
 });
